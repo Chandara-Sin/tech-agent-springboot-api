51710e664df3c2a95eca1618291e139d270a0142
Chandara-Sin <66314482+Chandara-Sin@users.noreply.github.com>
2022-04-27 22:33:17 +0700

    refactor: add postmapping employee

--- src/main/java/com/scd/tech_agent/controller/EmployeesController.java
+++ src/main/java/com/scd/tech_agent/controller/EmployeesController.java
@@ -8,7 +8,6 @@ import javax.validation.Valid;
 import com.scd.tech_agent.exception.DataNotFound;
 import com.scd.tech_agent.model.Employees;
 import com.scd.tech_agent.repository.EmployeesRepository;
-import com.scd.tech_agent.util.Gender;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -19,30 +18,40 @@ public class EmployeesController {
     @Autowired
     EmployeesRepository employeesRepo;
 
-    public String convertGender(Gender gender) {
-        if (gender.equals(Gender.not_known))
-            return "0";
-        else if (gender.equals(Gender.male)) {
-            return "1";
-        } else if (gender.equals(Gender.female)) {
-            return "2";
-        } else {
-            return "9";
-        }
-    }
+    // public String convertGender(Gender gender) {
+    // if (gender.equals(Gender.not_known))
+    // return "0";
+    // else if (gender.equals(Gender.male)) {
+    // return "1";
+    // } else if (gender.equals(Gender.female)) {
+    // return "2";
+    // } else {
+    // return "9";
+    // }
+    // }
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping("/addemployee")
-    public Integer addEmployee(@Valid @RequestBody Employees dataparam) {
-        // Employees employee = new Employees();
-        String first_name = dataparam.getFirst_name();
-        String last_name = dataparam.getLast_name();
-        String gender = convertGender(dataparam.getGender());
-        String hire_date = dataparam.getHire_date();
-        Integer salr_id = dataparam.getSalr_id();
-        Integer dept_id = dataparam.getDept_id();
-        Integer postn_id = dataparam.getPostn_id();
-        return employeesRepo.addEmployee(first_name, last_name, gender, hire_date, salr_id, dept_id, postn_id);
+    public Employees addEmployee(@Valid @RequestBody Employees dataparam) {
+        // String first_name = dataparam.getFirst_name();
+        // String last_name = dataparam.getLast_name();
+        // String gender = convertGender(dataparam.getGender());
+        // String hire_date = dataparam.getHire_date();
+        // Integer salr_id = dataparam.getSalr_id();
+        // Integer dept_id = dataparam.getDept_id();
+        // Integer postn_id = dataparam.getPostn_id();
+        // Integer is_success = employeesRepo.addEmployee(first_name, last_name, gender,
+        // hire_date, salr_id, dept_id,
+        // postn_id);
+        // if (is_success != 0) {
+        // System.out.print(employeesRepo.getNewEmployee());
+        // Employees new_employee = employeesRepo.getNewEmployee();
+        // return new_employee;
+        // } else {
+        // Employees new_employee = employeesRepo.getNewEmployee();
+        // return new_employee;
+        // }
+        return employeesRepo.save(dataparam);
     }
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -50,8 +59,8 @@ public class EmployeesController {
     public Map<String, String> deleteEmployee(@PathVariable(value = "id") Integer emp_id) throws DataNotFound {
         Employees employee = employeesRepo.findById(emp_id)
                 .orElseThrow(() -> new DataNotFound("Not Found User with id : " + emp_id));
-        employeesRepo.delete(employee);
 
+        employeesRepo.delete(employee);
         Map<String, String> response = new HashMap<>();
         response.put("message", "User was deleted successfully");
         return response;
--- src/main/java/com/scd/tech_agent/repository/EmployeesRepository.java
+++ src/main/java/com/scd/tech_agent/repository/EmployeesRepository.java
@@ -3,21 +3,29 @@ package com.scd.tech_agent.repository;
 import com.scd.tech_agent.model.Employees;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
+// import org.springframework.data.jpa.repository.Modifying;
+// import org.springframework.data.jpa.repository.Query;
+// import org.springframework.data.repository.query.Param;
+// import org.springframework.transaction.annotation.Transactional;
 
 public interface EmployeesRepository extends JpaRepository<Employees, Integer> {
 
         // Wrap database operations in a transaction to prevent data corruption.
-        @Transactional
-        @Modifying
-        @Query(value = "INSERT INTO tb_employees (first_name,last_name,gender,hire_date,salr_id,dept_id,postn_id)" +
-                        "VALUES (:first_name,:last_name,:gender,:hire_date,:salr_id,:dept_id,:postn_id) ", nativeQuery = true)
-        Integer addEmployee(@Param("first_name") String first_name, @Param("last_name") String last_name,
-                        @Param("gender") String gender, @Param("hire_date") String hire_date,
-                        @Param("salr_id") Integer salr_id,
-                        @Param("dept_id") Integer dept_id, @Param("postn_id") Integer postn_id);
+        // @Transactional
+        // @Modifying
+        // @Query(value = "INSERT INTO tb_employees
+        // (first_name,last_name,gender,hire_date,salr_id,dept_id,postn_id)" +
+        // "VALUES
+        // (:first_name,:last_name,:gender,:hire_date,:salr_id,:dept_id,:postn_id) ",
+        // nativeQuery = true)
+        // Integer addEmployee(@Param("first_name") String first_name,
+        // @Param("last_name") String last_name,
+        // @Param("gender") String gender, @Param("hire_date") String hire_date,
+        // @Param("salr_id") Integer salr_id,
+        // @Param("dept_id") Integer dept_id, @Param("postn_id") Integer postn_id);
+
+        // @Query(value = "SELECT * FROM tb_employees ORDER BY emp_id DESC LIMIT 1",
+        // nativeQuery = true)
+        // Employees getNewEmployee();
 
 }
