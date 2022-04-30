1fd972f006836b4eb6bddc6ba2f5e6c1ee5380e8
Chandara-Sin <66314482+Chandara-Sin@users.noreply.github.com>
2022-04-30 16:40:23 +0700

    feat: add put mapping controller

--- src/main/java/com/scd/tech_agent/controller/EmployeesController.java
+++ src/main/java/com/scd/tech_agent/controller/EmployeesController.java
@@ -1,6 +1,7 @@
 package com.scd.tech_agent.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.validation.Valid;
@@ -8,34 +9,48 @@ import javax.validation.Valid;
 import com.scd.tech_agent.exception.DataNotFound;
 import com.scd.tech_agent.model.Employees;
 import com.scd.tech_agent.repository.EmployeesRepository;
+import com.scd.tech_agent.util.Gender;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/v1")
 public class EmployeesController {
 
     @Autowired
     EmployeesRepository employeesRepo;
 
-    // public String convertGender(Gender gender) {
-    // if (gender.equals(Gender.not_known))
-    // return "0";
-    // else if (gender.equals(Gender.male)) {
-    // return "1";
-    // } else if (gender.equals(Gender.female)) {
-    // return "2";
-    // } else {
-    // return "9";
-    // }
-    // }
+    public String convertGender(Gender gender) {
+        if (gender.equals(Gender.not_known))
+            return "not_known";
+        else if (gender.equals(Gender.male)) {
+            return "male";
+        } else if (gender.equals(Gender.female)) {
+            return "female";
+        } else {
+            return "not_application";
+        }
+    }
+
+    @GetMapping("/employees")
+    public List<Employees> getAllEmployees() {
+        return employeesRepo.findAll();
+    }
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
-    @PostMapping("/addemployee")
+    @PostMapping("/employee")
     public Employees addEmployee(@Valid @RequestBody Employees dataparam) {
+        // System.out.println(dataparam.getFirst_name());
+        // System.out.println(dataparam.getLast_name());
+        // System.out.println(dataparam.getGender());
+        // System.out.println(dataparam.getHire_date());
+        // System.out.println(dataparam.getSalr_id());
+        // System.out.println(dataparam.getDept_id());
         // String first_name = dataparam.getFirst_name();
         // String last_name = dataparam.getLast_name();
-        // String gender = convertGender(dataparam.getGender());
+        // String gender = dataparam.getGender();
         // String hire_date = dataparam.getHire_date();
         // Integer salr_id = dataparam.getSalr_id();
         // Integer dept_id = dataparam.getDept_id();
@@ -51,18 +66,38 @@ public class EmployeesController {
         // Employees new_employee = employeesRepo.getNewEmployee();
         // return new_employee;
         // }
+        // Employees employee = new Employees();
+
         return employeesRepo.save(dataparam);
     }
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
-    @DeleteMapping("/delemployee/{id}")
+    @PutMapping("/employee/{id}")
+    public ResponseEntity<Employees> updateEmployee(@PathVariable(value = "id") Integer emp_id,
+            @Valid @RequestBody Employees employeeDetails) throws DataNotFound {
+        Employees employee = employeesRepo.findById(emp_id)
+                .orElseThrow(() -> new DataNotFound("Employee not found for this id : " + emp_id));
+
+        employee.setFirst_name(employeeDetails.getFirst_name());
+        employee.setLast_name(employeeDetails.getLast_name());
+        employee.setGender(employeeDetails.getGender());
+        employee.setHire_date(employeeDetails.getHire_date());
+        employee.setSalr_id(employeeDetails.getSalr_id());
+        employee.setDept_id(employeeDetails.getDept_id());
+        employee.setPostn_id(employeeDetails.getPostn_id());
+        Employees updatedEmployee = employeesRepo.save(employee);
+        return ResponseEntity.ok(updatedEmployee);
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @DeleteMapping("/employee/{id}")
     public Map<String, String> deleteEmployee(@PathVariable(value = "id") Integer emp_id) throws DataNotFound {
         Employees employee = employeesRepo.findById(emp_id)
-                .orElseThrow(() -> new DataNotFound("Not Found User with id : " + emp_id));
+                .orElseThrow(() -> new DataNotFound("Employee not found for this id : " + emp_id));
 
         employeesRepo.delete(employee);
         Map<String, String> response = new HashMap<>();
-        response.put("message", "User was deleted successfully");
+        response.put("message", "Employee was deleted successfully");
         return response;
     }
 
