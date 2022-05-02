a8592991ccc0e7bf879875e3535d7e09f41cacd2
Chandara-Sin <66314482+Chandara-Sin@users.noreply.github.com>
2022-05-02 23:20:10 +0700

    refactor: add response entity and error exception

--- src/main/java/com/scd/tech_agent/controller/EmployeesController.java
+++ src/main/java/com/scd/tech_agent/controller/EmployeesController.java
@@ -5,11 +5,13 @@ import java.util.Map;
 
 import javax.validation.Valid;
 
+import com.scd.tech_agent.exception.DataInvalid;
 import com.scd.tech_agent.exception.DataNotFound;
 import com.scd.tech_agent.model.Employees;
 import com.scd.tech_agent.service.EmployeeService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,28 +23,29 @@ public class EmployeesController {
     EmployeeService employeeServ;
 
     @GetMapping("/employees")
-    public List<Employees> getAllEmployees() {
-        return employeeServ.getAllEmployees();
+    public ResponseEntity<List<Employees>> getAllEmployees() {
+        return new ResponseEntity<>(employeeServ.getAllEmployees(), HttpStatus.OK);
     }
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping("/employee")
-    public ResponseEntity<Employees> addEmployee(@Valid @RequestBody Employees dataRequest) {
-        return employeeServ.addEmployee(dataRequest);
+    public ResponseEntity<Employees> addEmployee(@Valid @RequestBody Employees dataRequest)
+            throws DataInvalid, Exception {
+        return new ResponseEntity<Employees>(employeeServ.addEmployee(dataRequest), HttpStatus.CREATED);
     }
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PutMapping("/employee/{id}")
     public ResponseEntity<Employees> updateEmployee(@PathVariable(value = "id") Integer emp_id,
-            @Valid @RequestBody Employees employeeDetails) throws DataNotFound {
-        return employeeServ.updateEmployee(emp_id, employeeDetails);
+            @Valid @RequestBody Employees employeeDetails) throws DataNotFound, DataInvalid, Exception {
+        return new ResponseEntity<>(employeeServ.updateEmployee(emp_id, employeeDetails), HttpStatus.OK);
     }
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @DeleteMapping("/employee/{id}")
     public ResponseEntity<Map<String, String>> deleteEmployee(@PathVariable(value = "id") Integer emp_id)
-            throws DataNotFound {
-        return employeeServ.deleteEmployee(emp_id);
+            throws DataNotFound, Exception {
+        return new ResponseEntity<>(employeeServ.deleteEmployee(emp_id), HttpStatus.OK);
     }
 
 }
