535a7fcce1b444778c9f40a33b10bf4c4928c809
Chandara-Sin <66314482+Chandara-Sin@users.noreply.github.com>
2022-05-08 20:26:00 +0700

    refactor: use equals for string checking

--- src/main/java/com/scd/tech_agent/util/Helpers.java
+++ src/main/java/com/scd/tech_agent/util/Helpers.java
@@ -1,24 +1,19 @@
 package com.scd.tech_agent.util;
 
-import com.scd.tech_agent.repository.EmployeesRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Helpers {
 
-    @Autowired
-    EmployeesRepository employeesRepo;
-
     public Boolean validateGender(String value) {
         Boolean is_valid = false;
         for (Gender all_gender : Gender.values()) {
-            if (all_gender.toString() != value) {
-                is_valid = false;
-                break;
-            } else
+            if (all_gender.toString().equals(value)) {
                 is_valid = true;
+            } else {
+                is_valid = true;
+                break;
+            }
         }
         return is_valid;
     }
