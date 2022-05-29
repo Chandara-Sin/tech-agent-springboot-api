0ea6930a16824bfd67ca25ad3dcfbec4254f22c8
Chandara-Sin <>
2022-05-29 17:57:49 +0700

    refactor: clean code

--- src/main/java/com/scd/tech_agent/TechAgentApplication.java
+++ src/main/java/com/scd/tech_agent/TechAgentApplication.java
@@ -1,17 +1,8 @@
 package com.scd.tech_agent;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//@ComponentScan({"com.scd.tech_agent.controller","com.scd.tech_agent.service", "com.scd.tech_agent.model.mapper",
-//        "com.scd.tech_agent.model.dto","com.scd.tech_agent.util","com.scd.tech_agent.repository",
-////        "com.scd.tech_agent.entity","com.scd.tech_agent.model"})
-//@EnableJpaRepositories("com.scd.tech_agent.repository")
-//@EntityScan("com.scd.tech_agent.entity")
 @SpringBootApplication
 public class TechAgentApplication {
 
