617b5a76f20d050a1a3cf37e249c309cf03fdb3d
Chandara-Sin <chandara388588@gmail.com>
2022-06-21 00:01:52 +0700

    refactor: add custom error response

--- src/main/java/com/scd/tech_agent/exception/GlobalExceptionHandler.java
+++ src/main/java/com/scd/tech_agent/exception/GlobalExceptionHandler.java
@@ -13,24 +13,38 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(DataNotFound.class)
-    public ResponseEntity<ErrorDetails> dataNotFoundException(DataNotFound ex, HttpServletRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getRequestURI(),
-                HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND.getReasonPhrase());
-        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    public ResponseEntity<Object> dataNotFoundException(DataNotFound ex, HttpServletRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(
+                LocalDateTime.now(), ex.getMessage(),
+                request.getRequestURI(), HttpStatus.NOT_FOUND.getReasonPhrase()
+        );
+        return new ResponseEntity<>(
+                ErrorResponse.responseGeneral(errorDetails, HttpStatus.NOT_FOUND.value()),
+                HttpStatus.NOT_FOUND
+        );
     }
 
     @ExceptionHandler(DataInvalid.class)
-    public ResponseEntity<ErrorDetails> dataDuplicateException(DataInvalid ex, HttpServletRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getRequestURI(),
-                HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.getReasonPhrase());
-        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Object> dataDuplicateException(DataInvalid ex, HttpServletRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(
+                LocalDateTime.now(), ex.getMessage(),
+                request.getRequestURI(), HttpStatus.BAD_REQUEST.getReasonPhrase()
+        );
+        return new ResponseEntity<>(
+                ErrorResponse.responseGeneral(errorDetails, HttpStatus.BAD_REQUEST.value()),
+                HttpStatus.BAD_REQUEST
+        );
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorDetails> globalExceptionHandler(Exception ex, HttpServletRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(),
-                request.getRequestURI(), HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
-        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<Object> globalExceptionHandler(Exception ex, HttpServletRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(
+                LocalDateTime.now(), ex.getMessage(),
+                request.getRequestURI(), HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase()
+        );
+        return new ResponseEntity<>(
+                ErrorResponse.responseGeneral(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR.value()),
+                HttpStatus.INTERNAL_SERVER_ERROR
+        );
     }
 }
